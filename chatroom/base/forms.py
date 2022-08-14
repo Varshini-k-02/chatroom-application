35edffb5ec322724a1cd048aa3aef70a69df8020
varshini <varshini@gmail.com>
2022-08-14 18:13:25 +0530

    Adding files

---
+++ chatroom/base/forms.py
@@ -0,0 +1,15 @@
+from django.forms import ModelForm
+from .models import Room
+from django.contrib.auth.models import User
+
+class RoomForm(ModelForm):
+    class Meta:
+        model = Room
+        #Includes all the attributes from the Room model
+        fields ='__all__' 
+        exclude=['host','participants']
+        
+class UserForm(ModelForm):
+    class Meta:
+        model= User
+        fields = ['username','email']
