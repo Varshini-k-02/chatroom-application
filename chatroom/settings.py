b1032bd777e8fefebcee9b96ead4df528f059b07
Varshini K <78270481+Varshini-k-02@users.noreply.github.com>
2022-08-21 08:34:45 +0530

    Update settings.py

--- chatroom/settings.py
+++ chatroom/settings.py
@@ -21,8 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j$y2ask$ntvrqyhmcnauc12-p#z08c(te_xpb)e=7q*#g=una@'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
